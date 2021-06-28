1f0a7b33ee4c188ce0afa872e138a4ccbb2e0764
Peter Ledbrook <peter@cacoethes.co.uk>
2021-06-28 16:03:26 +0100

    Remove default config from build batch file

--- build-lwotc.bat
+++ build-lwotc.bat
@@ -11,4 +11,4 @@ if "%XCOM2GAMEPATH%" == "" (
 )
 
 rem The trailing backslash after %~dp0 is important, otherwise PowerShell thinks the " is being escaped!
-powershell.exe -NonInteractive -ExecutionPolicy Unrestricted  -file "%~dp0.scripts\build.ps1" -srcDirectory "%~dp0\" -sdkPath "%XCOM2SDKPATH%" -gamePath "%XCOM2GAMEPATH%" -config "default" %*
+powershell.exe -NonInteractive -ExecutionPolicy Unrestricted  -file "%~dp0.scripts\build.ps1" -srcDirectory "%~dp0\" -sdkPath "%XCOM2SDKPATH%" -gamePath "%XCOM2GAMEPATH%" %*
